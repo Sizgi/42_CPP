38c19e4401315475c3b3d2a266c60ec585c7ba0a
Samet Izgi <sizgi@student.42vienna.com>
2026-04-29 14:34:57 +0200

    ex02 some changes, added struct to vector instead of the int values

--- ex02/PmergeMe.cpp
+++ ex02/PmergeMe.cpp
@@ -6,14 +6,16 @@
 /*   By: sizgi <sizgi@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/21 14:57:25 by sizgi             #+#    #+#             */
-/*   Updated: 2026/04/21 18:51:11 by sizgi            ###   ########.fr       */
+/*   Updated: 2026/04/29 14:33:25 by sizgi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PmergeMe.hpp"
 
-PmergeMe::PmergeMe() {}
-
+PmergeMe::PmergeMe() {
+	myLevel = 0;
+	groupSize = 1;
+}
 PmergeMe::~PmergeMe () {}
 
 PmergeMe::PmergeMe(const PmergeMe &) {}
@@ -22,28 +24,90 @@ PmergeMe &PmergeMe::operator=(const PmergeMe &) {
 	return *this;
 }
 
+// void PmergeMe::myInsertFunc() {
+// 	size_t i = 0;
+// 	size_t	ax = (i*groupSize*2)+(groupSize*2)-1;
+// 	size_t	bx = (i*groupSize*2)+(groupSize)-1;
+// }
+
+void PmergeMe::myRecFunc(size_t counter) {
+	size_t save = 0;
+	groupSize = 1 << counter;
+	std::cout << "groupSize: " << GREEN << groupSize << RESET << " myLevel: " << GREEN << myLevel << RESET << std::endl;
+	for(size_t i = groupSize-1; i+groupSize < vectorSize; i += groupSize*2) {
+		if((i+groupSize < vectorSize) && myVector[i].numVal > myVector[i+groupSize].numVal) {
+			size_t groupStart = i - groupSize + 1;
+			for(size_t s = 0; s < groupSize; s++) {
+				std::swap(myVector[groupStart+s], myVector[(groupStart+s)+groupSize]);
+			}
+		}
+	}
+	for(std::vector<Numberdata>::iterator it = myVector.begin(); it != myVector.end(); it++) {
+		std::cout << it->numVal << " ";
+	}
+	std::cout << "save: " << save << std::endl;
+	std::cout << std::endl;
+	counter++;
+	if(counter < myPowerOfTwo)
+	{
+		myLevel = counter;
+		myRecFunc(counter);
+	}
+	
+	counter--;
+	std::cout << "IM HEERE" << std::endl;
+	// myInsertFunc();
+}
+
 void PmergeMe::mergerFunc(std::string givenStr) {
 	std::istringstream iss(givenStr);
-	int temp;
-	while(iss >> temp) {
+	Numberdata temp;
+	while(iss >> temp.numVal) {
+		// temp.pairIndex = NULL;
+		// temp.groupName = "empty";
 		myVector.push_back(temp);
 	}
 	vectorSize = myVector.size();
-	for(std::vector<int>::iterator it = myVector.begin(); it != myVector.end(); it++) {
-		std::cout << *it << " ";
+	for(std::vector<Numberdata>::iterator it = myVector.begin(); it != myVector.end(); it++) {
+		std::cout << it->numVal << " ";
 	}
 	std::cout << std::endl;
-	// size_t groupSize = 0;
-	// temp = vectorSize/2;
-	size_t powerOfTwo = (size_t)floor(log((double)vectorSize) / log(2.0));
-	std::cout << powerOfTwo << std::endl;
-	for(size_t i = 0; i < vectorSize; i+=2) {
-		std::cout << i << std::endl;
-		if((i + 1 < vectorSize) && myVector[i] > myVector[i+1])
-			std::vector<int>swap(myVector[i], myVector[i+1]);
+	myPowerOfTwo = (size_t)floor(log((double)vectorSize) / log(2.0));
+	std::cout << myPowerOfTwo << std::endl;
+	size_t counter = 0;
+	myRecFunc(counter);
+	if(vectorSize > 1 << myPowerOfTwo) {
+		std::cout << "power: " << (1 << myPowerOfTwo) << " size: " << vectorSize <<std::endl;
+		nonGroupStart = 1 << myPowerOfTwo;
 	}
-	for(std::vector<int>::iterator it = myVector.begin(); it != myVector.end(); it++) {
-		std::cout << *it << " ";
-	}
-	std::cout << std::endl;
+	// main is b1 and the a groups
+	// pend b groups exept b1(b is small side of the groups)
+	//
 }
+
+
+// static size_t JacobS(size_t givenNumber) {
+// 	size_t tempVal = 0;
+
+// 	tempVal =((1 << givenNumber)-static_cast<size_t>(pow(-1, givenNumber)))/3;
+// 	return (tempVal);
+// }
+
+
+
+
+// 2 11 0 17-9 18 14 19
+
+
+// rec 3 => 6 15 8 16(b1) - 2 11 0 17(a1) - 9 18 14 19(b2) - 3 10 1 21(a2) - (5 12 4 20)(b3) - 7 13
+
+// main: 6 15 8 16(b1) - 2 11 0 17(a1) - 3 10 1 21(a2) (b1 ve tüm a'lar main)
+
+// pend: 9 18 14 19(b2) - 5 12 4 20(b3) (b1 disindaki tüm b'ler)
+// non-participate: 7 13
+
+// insertion;
+// b3 from pend -> main
+// compare the biggest number vs other big numbers in main 
+// main was b1 a1 a2 => now b1 a1 b
+
--- ex02/PmergeMe.hpp
+++ ex02/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: sizgi <sizgi@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/21 14:57:34 by sizgi             #+#    #+#             */
-/*   Updated: 2026/04/21 18:40:20 by sizgi            ###   ########.fr       */
+/*   Updated: 2026/04/29 14:25:04 by sizgi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,29 @@
 #include<algorithm>
 #include <cmath>
 
+
+
+#define RED		"\033[31m"
+#define WARN	"\033[3;91m"
+#define WARN2	"\033[3;92m"
+#define WARN3	"\033[3;93m"
+#define WARN4	"\033[3;94m"
+#define GREEN	"\033[32m"
+#define YELLOW	"\033[33m"
+#define BLUE	"\033[34m"
+#define MAGENT	"\033[35m"
+#define CYAN	"\033[36m"
+#define GRAY	"\033[90m"
+#define BG		"\033[105m"
+#define RESET	"\033[0m"
+
+
+struct Numberdata {
+	int numVal;
+	int *pairIndex;
+	std::string groupName;
+};
+
 class PmergeMe {
 	public:
 		PmergeMe();
@@ -28,6 +51,12 @@ class PmergeMe {
 	private:
 		PmergeMe(const PmergeMe &copyThis);
 		PmergeMe &operator=(const PmergeMe &copyThis);
-		std::vector<int> myVector;
+		std::vector<Numberdata> myVector;
+		void myRecFunc(size_t counter);
+		void myInsertFunc();
 		size_t vectorSize;
+		size_t myPowerOfTwo;
+		size_t myLevel;
+		size_t nonGroupStart;
+		size_t groupSize;
 };
